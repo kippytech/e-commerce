270d041d5755aa3f4510a66445775b3a29e22a89
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-01-13 23:51:20 +0300

    add complete Manage Orders page ui & fubctionality + api

--- app/admin/manage-orders/page.tsx
+++ app/admin/manage-orders/page.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Container from '@/app/components/Container'
+import ManageOrdersClient from './ManageOrdersClient'
+import { getCurrentUser } from '@/actions/getCurrentUser'
+import NullData from '@/app/components/NullData'
+import getOrders from '@/actions/getOrders'
 
-function ManageOrders() {
+async function ManageOrders() {
+  const orders = await getOrders()
+  const currentUser = await getCurrentUser()
+
+  if (!currentUser || currentUser.role !== 'ADMIN') {
+    return <NullData title='Oops! Access Denied' />
+  }
+
   return (
-    <div>ManageOrders</div>
+    <div className='pt-8'>
+      <Container>
+        <ManageOrdersClient orders={orders} />
+      </Container>
+    </div>
   )
 }
 

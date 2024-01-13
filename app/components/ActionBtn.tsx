bb6d2dbcbb217879e4cfdec6527fddede989de26
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-01-13 16:33:48 +0300

    add => (had forgotten) haha

--- app/components/ActionBtn.tsx
+++ app/components/ActionBtn.tsx
@@ -6,7 +6,7 @@ type ActionBtnProps = {
     disabled?: boolean
 }
 
-const ActionBtn = ({ icon: Icon, onClick, disabled }: ActionBtnProps) {
+const ActionBtn = ({ icon: Icon, onClick, disabled }: ActionBtnProps) => {
     return (
         <button className={`flex items-center justify-center rounded cursor-pointer w-[40px] h-[30px] text-slate-700 border border-slate-400  ${disabled && 'opacity-50 cursor-not-allowed'}`} disabled={disabled} onClick={onClick}>
             <Icon size={18} />

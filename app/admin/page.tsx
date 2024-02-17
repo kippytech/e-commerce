63d496d6171da31e58c5a05c8e36ba3210b9763e
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-02-17 11:03:05 +0300

    protected summary page of admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,11 +2,19 @@ import Summary from "../components/Summary";
 import getOrders from "@/actions/getOrders";
 import getProducts from "@/actions/getProducts";
 import getUsers from "@/actions/getUsers";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 import Container from "../components/Container";
 import BarGraph from "../components/BarGraph";
 import getGraph from "@/actions/getGraph";
+import NullData from "../components/NullData";
 
 async function Admin() {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="Oops! Access Denied" />;
+  }
+
   const products = await getProducts({ category: null });
   const orders = await getOrders();
   const users = await getUsers();

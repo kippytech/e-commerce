aae3d143358f63936054855e6547045799c5d197
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-02-18 19:31:43 +0300

    search functionality mod

--- actions/getProducts.ts
+++ actions/getProducts.ts
@@ -1,56 +1,55 @@
-import prisma from '@/libs/prismadb'
+import prisma from "@/libs/prismadb";
 
-export type IProductParams ={
-    category?: string | null
-    searchTerm?: string | null
-}
+export type IProductParams = {
+  category?: string | null;
+  searchTerm?: string | null;
+};
 
-export default async  function getProducts (params: IProductParams) {
-    try {
-        const { category, searchTerm } = params
-        let searchString = searchTerm
+export default async function getProducts(params: IProductParams) {
+  try {
+    const { category, searchTerm } = params;
+    let searchString = searchTerm;
 
-        if (!searchTerm) {
-            searchString = ''
-        }
-
-        let query: any = {}
-
-        if (category) {
-            query.category = category
-        }
-
-        const products = await prisma.product.findMany({
-            where: {
-                ...query,
-                OR: [
-                    {
-                        name: {
-                            contains: searchString,
-                            mode: 'insensitive'
-                        },
-                        description: {
-                            contains: searchString,
-                            mode: 'insensitive'
-                        }
-                    }
-                ]
-            },
-            include: {
-                reviews: {
-                    include: {
-                        user: true
-                    },
-                    orderBy: {
-                        createdDate: 'desc'
-                    }
-                }
-            }
-        })
-
-        return products
-
-    } catch (error: any) {
-        throw new Error(error)
+    if (!searchTerm) {
+      searchString = "";
     }
+
+    let query: any = {};
+
+    if (category) {
+      query.category = category;
+    }
+
+    const products = await prisma.product.findMany({
+      where: {
+        ...query,
+        OR: [
+          {
+            name: {
+              contains: searchString,
+              mode: "insensitive",
+            },
+            // description: {
+            //     contains: searchString,
+            //     mode: 'insensitive'
+            // }
+          },
+        ],
+      },
+      include: {
+        reviews: {
+          include: {
+            user: true,
+          },
+          orderBy: {
+            createdDate: "desc",
+          },
+        },
+      },
+    });
+
+    return products;
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }

fad0eba625afb5e6b5484d18c6c3c4cfa0cbf8b1
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-02-19 10:23:31 +0300

    corrected prisma import error

--- app/api/product/[id]/route.ts
+++ app/api/product/[id]/route.ts
@@ -1,18 +1,22 @@
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import { NextResponse } from "next/server";
+import prisma from "@/libs/prismadb";
 
-export async function DELETE (request:Request, { params}: { params: { id: string } }) {
-    const currentUser = await getCurrentUser()
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const currentUser = await getCurrentUser();
 
-    if (!currentUser) return NextResponse.error()
-    
-    if (currentUser.role !== 'ADMIN') {
-        return NextResponse.error()
-    }
+  if (!currentUser) return NextResponse.error();
 
-    const product = await prisma?.product.delete({
-        where: { id: params.id }
-    })
+  if (currentUser.role !== "ADMIN") {
+    return NextResponse.error();
+  }
 
-    return NextResponse.json(product)
+  const product = await prisma?.product.delete({
+    where: { id: params.id },
+  });
+
+  return NextResponse.json(product);
 }

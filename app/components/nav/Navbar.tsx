6f2e33e503b90e93996dce0069b5768bbe240a1f
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-02-19 11:36:20 +0300

    searchbar for small screens

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -16,19 +16,24 @@ async function Navbar() {
     <div className="sticky top-0 z-30 w-full bg-slate-200 shadow-md">
       <div className="border-b-[1px] py-4">
         <Container>
-          <div className="flex items-center justify-between gap-3 md:gap-0">
-            <Link
-              className={`${redressed.className} text-2xl font-bold`}
-              href="/"
-            >
-              SokoMall
-            </Link>
-            <div className="hidden md:block">
-              <SearchBar />
+          <div className="flex flex-col items-center gap-2">
+            <div className="flex items-center justify-between gap-3 md:gap-0">
+              <Link
+                className={`${redressed.className} text-2xl font-bold`}
+                href="/"
+              >
+                SokoMall
+              </Link>
+              <div className="hidden md:block">
+                <SearchBar />
+              </div>
+              <div className="flex items-center gap-8 md:gap-12">
+                <CartCount />
+                <UserMenu currentUser={currentUser} />
+              </div>
             </div>
-            <div className="flex items-center gap-8 md:gap-12">
-              <CartCount />
-              <UserMenu currentUser={currentUser} />
+            <div className="block w-full md:hidden">
+              <SearchBar />
             </div>
           </div>
         </Container>

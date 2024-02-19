bcf7492921d4c2a217973948ef20a2641abb74b5
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-02-19 11:44:35 +0300

    fixing search

--- app/components/nav/SearchBar.tsx
+++ app/components/nav/SearchBar.tsx
@@ -1,49 +1,57 @@
-'use client'
+"use client";
 
-import { useRouter } from "next/navigation"
-import queryString from "query-string"
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
+import { useRouter } from "next/navigation";
+import queryString from "query-string";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 function SearchBar() {
+  const router = useRouter();
 
-    const router = useRouter()
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      searchTerm: "",
+    },
+  });
 
-    const { register, handleSubmit, reset, formState: {errors}} = useForm<FieldValues>({
-        defaultValues: {
-            searchTerm: ''
-        }
-    })
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    if (!data.searchTerm) return router.push("/");
 
-    const onSubmit: SubmitHandler<FieldValues> = async (data) => {
-        if (!data.searchTerm) return router.push('/')
+    const url = queryString.stringifyUrl(
+      {
+        url: "/",
+        query: {
+          searchTerm: data.searchTerm,
+        },
+      },
+      { skipNull: true },
+    );
 
-        const url = queryString.stringifyUrl({
-            url: '/',
-            query: {
-                searchTerm: data.searchTerm
-            }
-        }, {skipNull: true})
-
-        router.push(url)
-        reset()
-    }
+    router.push(url);
+    reset();
+  };
 
   return (
-    <div className='flex items-center'>
-        <input 
-          className='p-2 border border-gray-300 rounded-l-md focus:border-[0.5px] focus:border-slate-500 focus:outline-none w-80'
-          type='text'
-          placeholder='Explore SokoMall'
-          autoComplete="off"
-          {...register('searchTerm')}
-        />
-        <button className="p-2 rounded-r-md bg-slate-700 hover:opacity-80 text-white"
-          onClick={handleSubmit(onSubmit)}
-        >
-            Search
-        </button>
+    <div className="flex items-center">
+      <input
+        className="max-w-80 flex-grow rounded-l-md border border-gray-300 p-2 focus:border-[0.5px] focus:border-slate-500 focus:outline-none"
+        type="text"
+        placeholder="Explore SokoMall"
+        autoComplete="off"
+        {...register("searchTerm")}
+      />
+      <button
+        className="rounded-r-md bg-slate-700 p-2 text-white hover:opacity-80"
+        onClick={handleSubmit(onSubmit)}
+      >
+        Search
+      </button>
     </div>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
